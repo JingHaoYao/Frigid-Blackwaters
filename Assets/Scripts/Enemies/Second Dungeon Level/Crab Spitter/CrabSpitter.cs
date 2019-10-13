7351f665edb99796848e5b15127d7a99c88e9729
Jing Yao <jinghaoyao_@hotmail.com>
2019-10-12 23:05:32 -0400

    Implemented Crab Spitter enemy - fires three large slow moving blood balls which burst to in a spray added sprites for crab alpha, which is one of the two tier 4 enemies

---
+++ Assets/Scripts/Enemies/Second Dungeon Level/Crab Spitter/CrabSpitter.cs
@@ -0,0 +1,291 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabSpitter : Enemy
+{
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+    Rigidbody2D rigidBody2D;
+    public Sprite[] closedViews;
+    public GameObject spitterCrabShot;
+    public GameObject deadCrab;
+    GameObject playerShip;
+    float travelAngle;
+
+    private float foamTimer = 0;
+    public GameObject waterFoam;
+    List<AStarNode> path;
+    float pickSpritePeriod = 0;
+    int whatView = 0;
+    int mirror = 1;
+    public float travelSpeed = 1;
+    float attackPeriod = 0;
+    bool isAttacking = false;
+
+    BoxCollider2D hitBox;
+    float angleToShip = 0;
+
+    Vector3 targetPosition;
+
+    public GameObject invulnerableIcon;
+    public GameObject invulnerableHitBox;
+
+    float cardinalizeDirections(float angle)
+    {
+        if (angle > 22.5f && angle <= 67.5f)
+        {
+            return 45;
+        }
+        else if (angle > 67.5f && angle <= 112.5f)
+        {
+            return 90;
+        }
+        else if (angle > 112.5f && angle <= 157.5f)
+        {
+            return 135;
+        }
+        else if (angle > 157.5f && angle <= 202.5f)
+        {
+            return 180;
+        }
+        else if (angle > 202.5f && angle <= 247.5f)
+        {
+            return 225;
+        }
+        else if (angle > 247.5 && angle < 292.5)
+        {
+            return 270;
+        }
+        else if (angle > 292.5 && angle < 337.5)
+        {
+            return 315;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    void spawnFoam()
+    {
+        if (rigidBody2D.velocity.magnitude != 0)
+        {
+            float whatAngle = Mathf.Atan2(rigidBody2D.velocity.y, rigidBody2D.velocity.x) * Mathf.Rad2Deg;
+            foamTimer += Time.deltaTime;
+            if (foamTimer >= 0.05f * rigidBody2D.velocity.magnitude / 3f)
+            {
+                foamTimer = 0;
+                GameObject foam = Instantiate(waterFoam, transform.position, Quaternion.Euler(0, 0, whatAngle + 90));
+            }
+        }
+    }
+
+    void pickView(float angle)
+    {
+        if (angle > 255 && angle <= 285)
+        {
+            whatView = 2;
+            mirror = 1;
+        }
+        else if (angle > 285 && angle <= 360)
+        {
+            whatView = 1;
+            mirror = -1;
+        }
+        else if (angle > 180 && angle <= 255)
+        {
+            whatView = 1;
+            mirror = 1;
+        }
+        else if (angle > 75 && angle <= 105)
+        {
+            whatView = 4;
+            mirror = -1;
+        }
+        else if (angle >= 0 && angle <= 75)
+        {
+            whatView = 3;
+            mirror = 1;
+        }
+        else
+        {
+            whatView = 3;
+            mirror = -1;
+        }
+    }
+
+    void moveTowards(float direction)
+    {
+        rigidBody2D.velocity = new Vector3(Mathf.Cos(direction * Mathf.Deg2Rad), Mathf.Sin(direction * Mathf.Deg2Rad), 0) * travelSpeed;
+    }
+
+    IEnumerator attack()
+    {
+        targetPosition = pickRandPos();
+        animator.enabled = true;
+        invulnerableHitBox.SetActive(false);
+        invulnerableIcon.SetActive(false);
+        this.GetComponent<BoxCollider2D>().enabled = true;
+        for (int i = 0; i < 3; i++)
+        {
+            pickView(angleToShip);
+            int viewPicked = whatView;
+            float attackAngle = angleToShip;
+            animator.enabled = true;
+            animator.SetTrigger("Attack" + whatView.ToString());
+            yield return new WaitForSeconds(3f / 12f);
+            this.GetComponents<AudioSource>()[1].Play();
+            GameObject blast = Instantiate(spitterCrabShot, transform.position + new Vector3(Mathf.Cos(attackAngle * Mathf.Deg2Rad), Mathf.Sin(attackAngle * Mathf.Deg2Rad)) * 0.4f + Vector3.up * 0.7f, Quaternion.identity);
+            blast.GetComponent<CrabSpitterShot>().angleTravel = attackAngle * Mathf.Deg2Rad;
+            blast.GetComponent<ProjectileParent>().instantiater = this.gameObject;
+            yield return new WaitForSeconds(7f / 12f);
+        }
+        animator.enabled = false;
+
+        invulnerableHitBox.SetActive(true);
+        invulnerableIcon.SetActive(true);
+        this.GetComponent<BoxCollider2D>().enabled = false;
+        isAttacking = false;
+    }
+
+    Vector3 pickRandPos()
+    {
+        Vector3 currRandPos;
+        if(Random.Range(0,2) == 1)
+        {
+            if(Random.Range(0,2) == 1)
+            {
+                currRandPos = transform.position + new Vector3(Random.Range(3f, 5f), Random.Range(3f, 5f));
+            }
+            else
+            {
+                currRandPos = transform.position + new Vector3(Random.Range(-5f, -3f), Random.Range(3f, 5f));
+            }
+        }
+        else
+        {
+            if (Random.Range(0, 2) == 1)
+            {
+                currRandPos = transform.position + new Vector3(Random.Range(3f, 5f), Random.Range(-5f, -3f));
+            }
+            else
+            {
+                currRandPos = transform.position + new Vector3(Random.Range(-5f, -3f), Random.Range(-5f, -3f));
+            }
+        }
+
+        while (Physics2D.OverlapCircle(currRandPos, 0.5f) == true || Vector2.Distance(currRandPos, transform.position) < 4)
+        {
+            if (Random.Range(0, 2) == 1)
+            {
+                if (Random.Range(0, 2) == 1)
+                {
+                    currRandPos = transform.position + new Vector3(Random.Range(3f, 5f), Random.Range(3f, 5f));
+                }
+                else
+                {
+                    currRandPos = transform.position + new Vector3(Random.Range(-5f, -3f), Random.Range(3f, 5f));
+                }
+            }
+            else
+            {
+                if (Random.Range(0, 2) == 1)
+                {
+                    currRandPos = transform.position + new Vector3(Random.Range(3f, 5f), Random.Range(-5f, -3f));
+                }
+                else
+                {
+                    currRandPos = transform.position + new Vector3(Random.Range(-5f, -3f), Random.Range(-5f, -3f));
+                }
+            }
+        }
+        return currRandPos;
+    }
+
+    void travelLocation()
+    {
+        path = GetComponent<AStarPathfinding>().seekPath;
+        this.GetComponent<AStarPathfinding>().target = targetPosition;
+        AStarNode pathNode = path[0];
+        Vector3 targetPos = pathNode.nodePosition;
+        travelAngle = cardinalizeDirections((360 + Mathf.Atan2(targetPos.y - (transform.position.y + 0.4f), targetPos.x - transform.position.x) * Mathf.Rad2Deg) % 360);
+
+        if (isAttacking == false)
+        {
+            moveTowards(travelAngle);
+            pickView(travelAngle);
+        }
+        else
+        {
+            rigidBody2D.velocity = Vector3.zero;
+        }
+
+        if (Vector2.Distance(transform.position, path[path.Count - 1].nodePosition) < 0.6f && isAttacking == false && stopAttacking == false)
+        {
+            isAttacking = true;
+            StartCoroutine(attack());
+        }
+
+        transform.localScale = new Vector3(3.5f * mirror, 3.5f);
+        pickSpritePeriod += Time.deltaTime;
+
+        if (pickSpritePeriod >= 0.2f)
+        {
+            if (isAttacking == false)
+            {
+                spriteRenderer.sprite = closedViews[whatView - 1];
+            }
+            pickSpritePeriod = 0;
+        }
+    }
+
+    void Start()
+    {
+        hitBox = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        animator.enabled = false;
+        hitBox.enabled = false;
+        invulnerableIcon.SetActive(true);
+        invulnerableHitBox.SetActive(true);
+        playerShip = FindObjectOfType<PlayerScript>().gameObject;
+        attackPeriod = Random.Range(2f, 6f);
+        targetPosition = pickRandPos();
+    }
+
+    void Update()
+    {
+        angleToShip = (Mathf.Atan2(playerShip.transform.position.y - transform.position.y, playerShip.transform.position.x - transform.position.x) * Mathf.Rad2Deg + 360f) % 360f;
+        spawnFoam();
+        travelLocation();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<DamageAmount>() && health > 0)
+        {
+            int damageDealt = collision.gameObject.GetComponent<DamageAmount>().damage;
+            health -= damageDealt;
+            this.GetComponents<AudioSource>()[0].Play();
+            if (health <= 0)
+            {
+                GameObject dead = Instantiate(deadCrab, transform.position, Quaternion.identity);
+                addKills();
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                StartCoroutine(hitFrame());
+            }
+        }
+    }
+
+    IEnumerator hitFrame()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(.1f);
+        spriteRenderer.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/Enemies/Second Dungeon Level/Crab Spitter/CrabSpitterShot.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabSpitterShot : MonoBehaviour
+{
+    Animator animator;
+    float speed = 4;
+    public float angleTravel;
+    GameObject playerShip;
+    Camera mainCamera;
+    private bool wallCol;
+    float dissipatePeriod = 0;
+    public int damageDealing;
+    public GameObject smallSpitterShot;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        mainCamera = Camera.main;
+        playerShip = GameObject.Find("PlayerShip");
+    }
+
+    void summonSmallSpitterShot()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            GameObject smallShot = Instantiate(smallSpitterShot, transform.position, Quaternion.identity);
+            smallShot.GetComponent<ProjectileParent>().instantiater = this.GetComponent<ProjectileParent>().instantiater;
+            smallShot.GetComponent<AnemoneShot>().angleTravel = (i * 45) * Mathf.Deg2Rad;
+        }
+    }
+
+    void Update()
+    {
+        dissipatePeriod += Time.deltaTime;
+        if (wallCol == false)
+        {
+            transform.position += Time.deltaTime * speed * new Vector3(Mathf.Cos(angleTravel), Mathf.Sin(angleTravel), 0);
+            speed -= Time.deltaTime * 0.8f;
+            if (speed < 0)
+            {
+                speed = 0;
+            }
+        }
+
+        if (dissipatePeriod > 5f && wallCol == false)
+        {
+            this.GetComponent<AudioSource>().Play();
+            wallCol = true;
+            summonSmallSpitterShot();
+            animator.SetTrigger("Dissipate");
+            Destroy(this.gameObject, 0.333f);
+        }
+
+        if (transform.position.x < mainCamera.transform.position.x - 11f || transform.position.x > mainCamera.transform.position.x + 11f || transform.position.y > mainCamera.transform.position.y + 11f || transform.position.y < mainCamera.transform.position.y - 11f)
+        {
+            if (wallCol == false)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (wallCol == false)
+        {
+            this.GetComponent<AudioSource>().Play();
+            if (collision.gameObject.tag == "playerHitBox")
+            {
+                playerShip.GetComponent<PlayerScript>().amountDamage += damageDealing;
+            }
+            wallCol = true;
+            animator.SetTrigger("Dissipate");
+            Destroy(this.gameObject, 0.333f);
+        }
+    }
+}
