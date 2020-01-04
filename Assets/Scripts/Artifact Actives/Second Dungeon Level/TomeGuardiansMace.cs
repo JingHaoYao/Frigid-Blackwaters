7972ab0dc851c94b8b63c2b7e01f9669330cc677
Jing Yao <jinghaoyao_@hotmail.com>
2020-01-04 14:09:15 -0500

    Added examine dialogues and random dungeon entry dialogues for the second dungeon level

---
+++ Assets/Scripts/Artifact Actives/Second Dungeon Level/TomeGuardiansMace.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TomeGuardiansMace : ArtifactBonus
+{
+    DisplayItem displayItem;
+    Artifacts artifacts;
+    PlayerScript playerScript;
+    GameObject maceInstant;
+    public GameObject mace;
+    float macePeriod = 0;
+
+    void Start()
+    {
+        displayItem = GetComponent<DisplayItem>();
+        artifacts = GameObject.Find("PlayerShip").GetComponent<Artifacts>();
+        playerScript = GameObject.Find("PlayerShip").GetComponent<PlayerScript>();
+    }
+
+    void Update()
+    {
+        if (displayItem.isEquipped == true)
+        {
+            if(maceInstant == null)
+            {
+                macePeriod = Random.Range(0f, 2 * Mathf.PI);
+                maceInstant = Instantiate(mace, playerScript.transform.position + new Vector3(Mathf.Cos(macePeriod), Mathf.Sin(macePeriod)), Quaternion.identity);
+            }
+
+            macePeriod += Time.deltaTime * 2;
+            if(macePeriod > Mathf.PI * 2)
+            {
+                macePeriod = 0;
+            }
+            maceInstant.transform.position = playerScript.transform.position + new Vector3(Mathf.Cos(macePeriod), Mathf.Sin(macePeriod)) * 3;
+        }
+        else
+        {
+            if(maceInstant != null)
+            {
+                maceInstant.GetComponent<TomeGuardiansMaceInstant>().unequipArtifact();
+                maceInstant = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Artifact Actives/Second Dungeon Level/TomeGuardiansMaceInstant.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TomeGuardiansMaceInstant : MonoBehaviour
+{
+    CircleCollider2D circCol;
+    Animator animator;
+
+
+    bool isDestroying = false;
+
+    void Start()
+    {
+        circCol = GetComponent<CircleCollider2D>();
+        animator = GetComponent<Animator>();
+        circCol.enabled = false;
+    }
+
+    void Update()
+    {
+        if(!animator.GetCurrentAnimatorStateInfo(0).IsName("Tome Guardian's Mace Form") && isDestroying == false)
+        {
+            circCol.enabled = true;
+        }
+    }
+
+    public void unequipArtifact()
+    {
+        isDestroying = true;
+        circCol.enabled = false;
+        animator.SetTrigger("Burst");
+        GetComponent<AudioSource>().Play();
+        Destroy(this.gameObject, 0.333f);
+    }
+}
